8cf0cfe2489661efb92e7c08bec64996e8f68d21
matias0621 <132005393+matias0621@users.noreply.github.com>
2025-01-25 17:55:21 -0300

    IA basic ✅, I hope

--- P.O.P. game/Assets/Scripts/Enemy/EnemyIA.cs
+++ P.O.P. game/Assets/Scripts/Enemy/EnemyIA.cs
@@ -1,73 +1,112 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Serialization;
 
 public class EnemyIA : MonoBehaviour
 {
-    // Aca va el transform del player que es lo que vamos a perseguir
     public Transform Target;
-    // La distancia minima que debe haber entre el enemigo y player para que lo persiga
     public float FollowPlayerDistance;
-    // Los waypoint son los puntos que va a seguir el enemigo cuando no esta persiguiendo al jugador
     public Transform[] Waypoints;
-    
-    // Variables privadas para hacer la comprobaciones
+
     private NavMeshAgent agent;
+    private Animator animator;
     private float distance;
     private float waypointDistance;
-    private bool followPlayer;
-    private int i = 0;
-    
-    
+    private int currentWaypoint = 0;
+    private bool isAttacking = false;
+
+    private enum EnemyState { Patrolling, Chasing, Attacking, Resting }
+    private EnemyState currentState = EnemyState.Patrolling;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("move", true);
     }
 
-    
     void Update()
     {
-         // Obtenemos la distacia que hay entre el player y el enemigo en cada frame
-         distance = Vector3.Distance(agent.transform.position, Target.position);
-        
-         // Se verifica la distancia para perseguir al player
-         if (distance < FollowPlayerDistance)
-         {
-             followPlayer = true;
-         }
-         else
-         {
-             followPlayer = false;
-         }
+        distance = Vector3.Distance(agent.transform.position, Target.position);
 
-         if (followPlayer)
-         {
-             // Le decimos que se puede mover
-             agent.isStopped = false;
-             // Le pasamos la posicion del player para que vaya ahi
-             agent.SetDestination(Target.position);
-             // Le decimos a que velocidad tiene que ir
-             // Lo pongo asi para que no sea tan brusco el movimiento y sea mas suave
-             agent.speed = (transform.position - Target.position).magnitude;
-             
-             if (distance < 3) agent.isStopped = true;
-         }
-         else
-         {
-             // Le decimos a que waypoint de la lista tiene que ir
-             agent.SetDestination(Waypoints[i].position);
-             
-             // Le damos una velocidad para que se mueva
-             agent.speed = (transform.position - Waypoints[i].position).magnitude;
-             
-             // Obtenemos la distancia que hay entre el waypoint que estamos siguiendo y la posicion del enemigo
-             waypointDistance = Vector3.Distance(transform.position, Waypoints[i].position);
-             
-             // Si la distancia es menor a 1 empezamos a perseguir el siguiente waypoint
-             if (waypointDistance < 1) i++;
+        switch (currentState)
+        {
+            case EnemyState.Patrolling:
+                Patrol();
+                if (distance < FollowPlayerDistance)
+                {
+                    currentState = EnemyState.Chasing;
+                }
+                break;
 
-             // Reiniciamos la persecusion cuando llegamos al ultimo waypoint de la lista
-             if (i == Waypoints.Length) i = 0;
-         }
+            case EnemyState.Chasing:
+                ChasePlayer();
+                if (distance < 2.8f)
+                {
+                    currentState = EnemyState.Attacking;
+                    StartCoroutine(Attack());
+                }
+                else if (distance >= FollowPlayerDistance)
+                {
+                    currentState = EnemyState.Patrolling;
+                }
+                break;
+
+            case EnemyState.Attacking:
+                // Se maneja en la corrutina
+                break;
+
+            case EnemyState.Resting:
+                // No hacer nada mientras descansa
+                break;
+        }
+    }
+
+    void Patrol()
+    {
+        if (currentWaypoint >= Waypoints.Length) currentWaypoint = 0;
+
+        agent.isStopped = false;
+        agent.SetDestination(Waypoints[currentWaypoint].position);
+        waypointDistance = Vector3.Distance(transform.position, Waypoints[currentWaypoint].position);
+
+        if (waypointDistance < 3f)
+        {
+            StartCoroutine(Rest());
+        }
+    }
+
+    void ChasePlayer()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(Target.position);
+        animator.SetBool("move", true);
+    }
+
+    IEnumerator Attack()
+    {
+        currentState = EnemyState.Attacking;
+        agent.isStopped = true;
+        animator.SetBool("move", false);
+        animator.SetBool("Attack", true);
+
+        yield return new WaitForSeconds(1f);
+
+        animator.SetBool("Attack", false);
+        currentState = EnemyState.Chasing;
+    }
+
+    IEnumerator Rest()
+    {
+        currentState = EnemyState.Resting;
+        animator.SetBool("move", false);
+        agent.isStopped = true;
+
+        yield return new WaitForSeconds(3f);
+
+        currentWaypoint++;
+        currentState = EnemyState.Patrolling;
+        animator.SetBool("move", true);
+        agent.isStopped = false;
     }
 }

5acd77d78bed3a692d24d81a4493fa7e1e5c7848
matias0621 <132005393+matias0621@users.noreply.github.com>
2025-01-25 11:45:58 -0300

    First Commit

---
+++ P.O.P. game/Assets/Scripts/Enemy/EnemyIA.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class EnemyIA : MonoBehaviour
+{
+    public Transform Target;
+    public float AttackDistance;
+    
+    private NavMeshAgent agent;
+    private float distance;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+         distance = Vector3.Distance(agent.transform.position, Target.position);
+
+         if (distance < AttackDistance)
+         {
+             agent.isStopped = true;
+         }
+         else
+         {
+             agent.isStopped = false;
+             agent.SetDestination(Target.position);
+         }
+    }
+}

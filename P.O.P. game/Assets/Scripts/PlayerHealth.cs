0d5317380cac0a741b0c5b81b9a57deb69df2140
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-01-26 14:17:43 -0300

    Update PlayerHealth.cs

--- P.O.P. game/Assets/Scripts/PlayerHealth.cs
+++ P.O.P. game/Assets/Scripts/PlayerHealth.cs
@@ -16,7 +16,7 @@ public class PlayerHealth : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        health = 80;
+        health = maxHealth;
     }
 
     // Update is called once per frame

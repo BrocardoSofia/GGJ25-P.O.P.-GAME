a4e2bc87e276bce1c4c22511da74be9f6d97febd
BrocardoSofia <sofia.brocardo@hotmail.com>
2025-01-25 22:04:36 -0300

    Credits Scene Update

--- P.O.P. game/Assets/Scripts/CreditScript.cs
+++ P.O.P. game/Assets/Scripts/CreditScript.cs
@@ -3,6 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class CreditScript : MonoBehaviour
 {
+    private Animator animator;
+    private bool hasPlayed = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.Play("CreditsAnimation", 0, 0f);
+    }
+
+    void Update()
+    {
+        // Verifica si la animación ha terminado de reproducirse
+        if (!animator.GetCurrentAnimatorStateInfo(0).loop &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            if (!hasPlayed)
+            {
+                hasPlayed = true;
+                // Pausa la animación en el último fotograma
+                animator.speed = 0f;
+            }
+        }
+    }
     public void OnCreditAnimationEnd()
     {
         SceneManager.LoadScene(0);
